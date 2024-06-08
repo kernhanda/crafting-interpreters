3c8f05b90f9bd311afba911bcb1662306164262d
Kern Handa <kern.handa@gmail.com>
2024-06-08 16:26:49 -0700

    Finishing up the scanner

--- python/plox/scanner.py
+++ python/plox/scanner.py
@@ -1,10 +1,29 @@
-from typing import Any, List, Optional
+from typing import Any, Dict, List, Optional
 
 from plox.token import Token, TokenType
 from plox.utils import error
 
 
 class Scanner:
+    Keywords: Dict[str, TokenType] = {
+        "and": TokenType.AND,
+        "class": TokenType.CLASS,
+        "else": TokenType.ELSE,
+        "false": TokenType.FALSE,
+        "for": TokenType.FOR,
+        "fun": TokenType.FUN,
+        "if": TokenType.IF,
+        "nil": TokenType.NIL,
+        "or": TokenType.OR,
+        "print": TokenType.PRINT,
+        "return": TokenType.RETURN,
+        "super": TokenType.SUPER,
+        "this": TokenType.THIS,
+        "true": TokenType.TRUE,
+        "var": TokenType.VAR,
+        "while": TokenType.WHILE,
+    }
+
     def __init__(self, source: str) -> None:
         self.source = source
         self.tokens: List[Token] = []
@@ -82,6 +101,10 @@ class Scanner:
                 self.line += 1
             case '"':
                 self.string()
+            case c if str.isdigit(c):
+                self.number()
+            case c if str.isidentifier(c):
+                self.identifier()
             case _:
                 error(self.line, "Unexpected character.")
 
@@ -100,6 +123,12 @@ class Scanner:
 
         return self.source[self.current]
 
+    def peekNext(self) -> str:
+        if (self.current + 1) >= len(self.source):
+            return "\0"
+
+        return self.source[self.current + 1]
+
     def string(self):
         while self.peek() != '"' and not self.is_at_end():
             if self.peek() == "\n":
@@ -110,9 +139,29 @@ class Scanner:
             error(self.line, "Unterminated string.")
             return
 
-        # The closing ""
+        # The closing "
         self.advance()
 
         # Trim the surrounding quotes.
         value = self.source[self.start + 1 : self.current - 1]
         self.add_token(TokenType.STRING, value)
+
+    def number(self):
+        while self.peek().isdigit():
+            self.advance()
+
+        if self.peek() == "." and self.peekNext().isdigit():
+            self.advance()
+
+            while self.peek().isdigit():
+                self.advance()
+
+        self.add_token(TokenType.NUMBER, float(self.source[self.start : self.current]))
+
+    def identifier(self):
+        while self.peek().isalnum():
+            self.advance()
+
+        text = self.source[self.start : self.current]
+        token_type = self.Keywords.get(text, TokenType.IDENTIFIER)
+        self.add_token(token_type)
