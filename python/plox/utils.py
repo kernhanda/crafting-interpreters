eb06eed7c65b820f7d0f3432915036d0904d60a9
Kern Handa <kern.handa@gmail.com>
2024-06-08 01:51:04 -0700

    Update LoxError

--- python/plox/utils.py
+++ python/plox/utils.py
@@ -1,6 +1,11 @@
 class LoxError(Exception):
-    def __init__(self) -> None:
+    def __init__(self, line: int, err: str) -> None:
         super().__init__()
+        self.line = line
+        self.err = err
+
+    def __str__(self) -> str:
+        return f"Error at line {self.line}: {self.err}"
 
 
 def error(line: int, message: str):
