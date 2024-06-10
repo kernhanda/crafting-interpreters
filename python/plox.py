0e7961b10d046e15af998cebeb1b8847753787fb
Kern Handa <kern.handa@gmail.com>
2024-06-10 12:38:54 -0700

    Remove test

--- python/plox.py
+++ python/plox.py
@@ -33,14 +33,6 @@ def run_prompt():
 
 
 def main():
-    expr = Binary(
-        Unary(Token(TokenType.MINUS, "-", None, 1), Literal(123)),
-        Token(TokenType.STAR, "*", None, 1),
-        Grouping(Literal(45.67)),
-    )
-    print(AstPrinter().print(expr))
-    return
-
     args = sys.argv
     if len(args) > 2:
         print(f"Usage: {args[0]} [script]")
